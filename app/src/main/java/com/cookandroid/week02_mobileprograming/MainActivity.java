383c709766a8763ac6a9f89601eeb6864b2ea4c7
dongyoon9802 <dongyoon9802@gmail.com>
2021-09-15 09:22:40 +0900

    finished

--- app/src/main/java/com/cookandroid/week02_mobileprograming/MainActivity.java
+++ app/src/main/java/com/cookandroid/week02_mobileprograming/MainActivity.java
@@ -5,24 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
+    CheckBox chb_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.btn_num2);
+        btn1 = findViewById(R.id.btn_num1);
+        chb_1 = findViewById(R.id.chb_1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"번트눌름",Toast.LENGTH_SHORT).show();
             }
         });
-
+        chb_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"checked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
